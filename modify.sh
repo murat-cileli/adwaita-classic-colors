a5dde9fe61eb7c19c60680d3e5c6984c8e79dc6b
Murat Çileli <murat.cileli@gmail.com>
2019-07-05 13:21:15 +0300

    Update 2019-07-05 13:21

---
+++ modify.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+sed -i 's/black/#222/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#f6f5f4/#efefef/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#e1dedb/#efefef/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#dad6d2/#ececec/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#bfb8b1/#ccc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#cdc7c2/#ccc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#e5e3df/#e5e5e5/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#cfcac4/#dcdcdc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#c6bfb9/#ccc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#bab3ab/#ccc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#eae7e5/#e5e5e5/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#d5d0cb/#dcdcdc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#d6d1cd/#e5e5e5/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#fbfafa/#fbfbfb/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#faf9f8/#fafafa/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#eae8e6/#eaeaea/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#d5d0cc/#d5d5d5/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#ffffff/#fcfcfc/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#edebe9/#ebebeb/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#e6e3e0/#e5e5e5/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#dfdcd8/#dfdfdf/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/white/#fdfdfd/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#e3e3e3/#e6e6e6/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/to top, #ececec, #efefef/to top, #efefef, #e8e8e8/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/border-style: solid; border-color: #ccc;/border-style: solid; border-color: #d5d5d5;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/rgba(0, 0, 0, 0.5), 0 0 0 1px rgba(0, 0, 0, 0.23);/rgba(50, 50, 50, 0.25), 0 0 0 1px rgba(50, 50, 50, 0.1);/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/scrollbar { background-color: #cecece;/scrollbar { background-color: transparent;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/scrollbar.top { border-bottom: 1px solid #ccc;/scrollbar.top { border-bottom: none;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/scrollbar.bottom { border-top: 1px solid #ccc;/scrollbar.bottom { border-top: none;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/scrollbar.left { border-right: 1px solid #ccc;/scrollbar.left { border-right: none;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/scrollbar.right { border-left: 1px solid #ccc;/scrollbar.right { border-left: none;/g' ./gtk-3.0/gtk-contained.css
+sed -i 's/#070707/#1a1a1a/g' ./gtk-3.0/gtk-contained-dark.css
+
+
+
